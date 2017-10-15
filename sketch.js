05e25bf76f477394eec4b8596922c555d88aa064
Won Ryul Lee <wlee367@uwo.ca>
2017-10-15 01:43:27 -0400

    it now draws a line if you press/drag from the point, nowhere close to perfect, but I think its a good starting point

--- sketch.js
+++ sketch.js
@@ -1,97 +1,65 @@
-// All the paths
-var paths = [];
-// Are we painting?
-var painting = false;
-// How long until the next circle
-var next = 0;
-// Where are we now and where were we?
-var current;
-var previous;
-var song;
-var songArray = [];
-
-var paintColour;
-
+var point;
+var x = 0; //boolean :D
 function preload(){
 
 }
 function setup(){
-  createCanvas(800, 600);
+  cnv = createCanvas(800, 600);
 
 }
 
+/*https://p5js.org/reference/#/p5.Element/mouseClicked*/
+// ^ Hannah's changing colors method but probably a better way
 
-function paintRed(){
-    paintColour = "red"
-}
-function paintBlue(){
-    paintColour = "blue"
-}
-function paintGreen(){
-    paintColour = "green"
-}
 
 function draw(){
   background(200);
   stroke(0);
   strokeWeight(20);
 
-  point(mouseX,mouseY);
+    p1 = new pointClass(300,300);
+    p1.display();
+    p1.return_coordinates();
 
-  var x1 = 300;
-  var y1 = 300;
-  var x2 = 500;
-  var y2 = 200;
+    p2 = new pointClass(400, 500);
+    p2.display();
+    p2.return_coordinates();
 
-  p1= new pointClass(x1,y1);
-  p2= new pointClass(x2,y2);
-
-  p1.check(x1,y1);
-
-
-  beginShape();
-    
-  endShape();
-    
+    if(Boolean(x)){
+      drawLine(p1.x, p1.y, p2.x, p2.y);
+    }
      
 }
-
-function pointClass(x, y){
-  point(x,y);
-}
-pointClass.prototype.check = function(x,y){
-  var d = dist(mouseX,mouseY,x,y);
-  if (d < 25){
-      line(x,y, mouseX,mouseY);
+// point class
+function pointClass(x,y){
+  
+  this.return_coordinates = function(){
+    this.x = x;
+    this.y = y;
+  }
+  this.display = function(){
+    point(x,y);
   }
 }
 
-// Start it up
-function mousePressed() {
-  // next = 0;
-  // painting = true;
-  // previous.x = mouseX;
-  // previous.y = mouseY;
-
-  // // if(!song.isPlaying()){
-  // // 	song.play();
-  // // }
-}
-
 function mouseDragged(){
-	// if(!song.isPlaying() || song.isPaused()){
-	// 	song.play();
-	// }
-}
-// Stop
-function mouseReleased() {
-  // painting = false;
-  // if(song.isPlaying()){
-  // 	song.pause();
-  // }
+  d = dist(mouseX, mouseY, p1.x, p1.y);
+  e = dist(mouseX, mouseY, p2.x, p2.y);
+
+  var check = d-e;
+  if(check < 20){
+    //console.log('you prssed!');
+    x = 1; 
+  }
 }
 
-
+function drawLine(x1, y1, x2, y2){
+  //console.log('this function was activated');
+  stroke(0);
+  strokeWeight(20);
+  line(x1, y1, x2, y2); // why does this not fire????
+  //console.log('a line should have been drawn by now');
+}
 
 function windowResized(){
 	resizeCanvas(windowWidth, windowHeight);
