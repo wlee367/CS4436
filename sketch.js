bf62b089e51b64b9c9ee9abd90ce33c294dc340b
hrusswurm <hrusswur@uwo.ca>
2017-10-16 19:14:05 -0400

    Added ability to change colour

--- sketch.js
+++ sketch.js
@@ -1,21 +1,23 @@
 var point;
 var x = 0; //boolean :D
-function preload(){
 
+var r = 0; //RGB red
+var g = 0; //RGB green
+var b = 0; //RGB blue
+
+
+function preload(){
 }
+
 function setup(){
   cnv = createCanvas(800, 600);
-
 }
-
-/*https://p5js.org/reference/#/p5.Element/mouseClicked*/
-// ^ Hannah's changing colors method but probably a better way
-
 
 function draw(){
   background(200);
   stroke(0);
   strokeWeight(20);
+   
 
     p1 = new pointClass(300,300);
     p1.display();
@@ -42,6 +44,7 @@ function pointClass(x,y){
   }
 }
 
+
 function mouseDragged(){
   d = dist(mouseX, mouseY, p1.x, p1.y);
   e = dist(mouseX, mouseY, p2.x, p2.y);
@@ -53,14 +56,30 @@ function mouseDragged(){
   }
 }
 
+
 function drawLine(x1, y1, x2, y2){
   //console.log('this function was activated');
-  stroke(0);
+  stroke(r, g, b);
   strokeWeight(20);
   line(x1, y1, x2, y2); // why does this not fire????
   //console.log('a line should have been drawn by now');
 }
 
+function changeRed() {
+  r = 255; g = 0; b = 0;
+}
+
+function changeGreen() {
+  r = 0; g = 255; b = 0;
+}
+
+function changeBlue() {
+  r = 0; g = 0; b = 255;
+}
+
 function windowResized(){
 	resizeCanvas(windowWidth, windowHeight);
 }
+
+
+
